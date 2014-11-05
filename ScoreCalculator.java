daeb75d53044fa77239b4ea1b0a4312474d4fec0
Amine Bourdi <dgad>
2014-11-05 15:16:00 -0500

    initial

--- ScoreCalculator.java
+++ ScoreCalculator.java
@@ -1,17 +1,29 @@
 
 public class ScoreCalculator {
-/*
+/**
+ * 
+ * @author Amine
+ * Created: November 1, 2014
+ * Purpose: Driver that's called upon in ScoreGui
  * 
  */
 public static double calculateAvgScore (double [] testScores, double [] testWeights) {
+		/*
+		 * Initializing weighted score
+		 */
 		double weightedScore=0;
-		
+		/*
+		 * multiplying weights by test scores to find the weighted score
+		 */
 		for (int i=0; i<testScores.length; i++) {
 			
 			weightedScore += testScores[i] * testWeights[i];
 			
 		}
 		
+		/*
+		 * returning the weighted score
+		 */
 		return weightedScore;
 				
 	}
--- ScoreGui.java
+++ ScoreGui.java
@@ -3,11 +3,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
-
+/**
+ * 
+ * @author Amine
+ * Created: November 1, 2014
+ * Purpose: Used to find the Weighted score of 4 tests
+ *
+ */
 public class ScoreGui extends JFrame implements ActionListener {
 	
-	/**
-	 * 
+	/*
+	 * All the initializations for the variables
 	 */
 	private static final long serialVersionUID = 629353668517864690L;
 	JTextField jTxtScore1;
@@ -26,82 +32,137 @@ public class ScoreGui extends JFrame implements ActionListener {
 	JLabel jLblWeights;
 	JTextField jTxtWeightedScore;
 	
+	/*
+	 * beginning of ScoreGui
+	 */
 	public ScoreGui () {
 		
+		/*
+		 * creating new panel for scores and setting GridLayout and Color
+		 */
 		jPnScores = new JPanel();
 		jPnScores.setLayout(new GridLayout (5, 1));
 		jPnScores.setBackground(Color.GRAY);
 		
+		/*
+		 * creating new panel for weights and setting GridLayout and Color
+		 */
 		jPnWeights = new JPanel();
 		jPnWeights.setLayout(new GridLayout (5, 1));
 		jPnWeights.setBackground(Color.GRAY);
 		
+		/*
+		 * creating new panel for the button
+		 */
 		jPnButton = new JPanel();
 		jPnButton.setBackground(Color.GRAY);
 		
+		/*
+		 * Creating new label for the frame
+		 */
 		jLblScores = new JLabel("Test Scores");
 		jPnScores.add(jLblScores);
 		
+		/*
+		 * setting length for the text fields
+		 */
 		jTxtScore1 = new JTextField(20);	
 		jTxtScore2 = new JTextField(20);	
 		jTxtScore3 = new JTextField(20);	
 		jTxtScore4 = new JTextField(20);
-				
+		
+		/*
+		 * adding text fields to the panel
+		 */
 		jPnScores.add(jTxtScore1);
 		jPnScores.add(jTxtScore2);
 		jPnScores.add(jTxtScore3);
 		jPnScores.add(jTxtScore4);
 		
+		/*
+		 * creating label for frame and adding it to the panel
+		 */
 		jLblWeights = new JLabel("Test Weights");
 		jPnWeights.add(jLblWeights);
 		
+		/*
+		 * setting length for text fields
+		 */
 		jTxtWeight1 = new JTextField(20);
 		jTxtWeight2 = new JTextField(20);
 		jTxtWeight3 = new JTextField(20);
 		jTxtWeight4 = new JTextField(20);
 		
+		/*
+		 * adding weight text fields to the panel
+		 */
 		jPnWeights.add(jTxtWeight1);
 		jPnWeights.add(jTxtWeight2);
 		jPnWeights.add(jTxtWeight3);
 		jPnWeights.add(jTxtWeight4);
 		
+		/*
+		 * adding panels to the frame and arranging them
+		 */
 		add(jPnScores, BorderLayout.WEST);
 		add(jPnWeights, BorderLayout.EAST);
 		add(jPnButton, BorderLayout.SOUTH);
 		
+		/*
+		 * adding buttons to panel
+		 */
 		jTxtWeightedScore = new JTextField(30);
 		jPnButton.add(jTxtWeightedScore);
 		jBtnCalculateButton = new JButton("Calculate Average");
 		jPnButton.add(jBtnCalculateButton);
+		
+		/*
+		 * telling Action listener to listn to this input
+		 */
 		jBtnCalculateButton.addActionListener(this);
 		
+		/*
+		 * making the Gui visible and packing it
+		 */
 		setVisible(true);
-		//setSize(new Dimension(500,500));
 		pack ();
 		
 	}
-	
+	/*
+	 * Method to listen to the button
+	 */
 	public void actionPerformed(ActionEvent e) {
-			
+		
+		/*
+		 * creating an array for test scores
+		 */
 		double [] testScores = new double[4];
 		testScores[0] = Double.parseDouble(jTxtScore1.getText());
 		testScores[1] = Double.parseDouble(jTxtScore2.getText());
 		testScores[2] = Double.parseDouble(jTxtScore3.getText());
 		testScores[3] = Double.parseDouble(jTxtScore4.getText());
 		
+		/*
+		 * creating an array for test weights
+		 */
 		double [] testWeights = new double[4];
 		testWeights[0] = Double.parseDouble(jTxtWeight1.getText());
 		testWeights[1] = Double.parseDouble(jTxtWeight2.getText());
 		testWeights[2] = Double.parseDouble(jTxtWeight3.getText());
 		testWeights[3] = Double.parseDouble(jTxtWeight4.getText());
 		
+		/*
+		 * setting text for the text field where the weighted sscore will be displayed
+		 */
 		jTxtWeightedScore.setText("The weighted Score is: " + 
 				ScoreCalculator.calculateAvgScore(testScores, testWeights));
 		
 			}
 		
 
-	
+	/*
+	 * main method
+	 */
 	public static void main(String[] args) {
 	new ScoreGui();	
 
